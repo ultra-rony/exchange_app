08adb706848bc7702f71da7399f6fc83914866c7
Roman Diakonov <vegappunk@gmail.com>
2025-04-07 22:23:59 +0300

    chore: setup env

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Constants {
   static const String baseUrlV3 = "https://rest.coincap.io/v3/";
+  static String? coincapApiKey = dotenv.env['COINCAP_API_KEY'];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:exchange_app/core/app/app.dart';
 import 'package:exchange_app/core/di/injectable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await configureInjection();
   runApp(const App());
