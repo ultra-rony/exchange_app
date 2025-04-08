776d4f26de1002fea1721872c9a6f989a9f85847
Roman Diakonov <vegappunk@gmail.com>
2025-04-08 06:51:58 +0300

    refactor: rm auto_route

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,34 +1,44 @@
-import 'package:exchange_app/core/app/routes/app_routers.dart';
-import 'package:exchange_app/core/app/themes/dark_theme.dart';
 import 'package:exchange_app/core/app/themes/light_theme.dart';
+import 'package:exchange_app/core/di/injectable.dart';
 import 'package:exchange_app/generated/l10n.dart';
+import 'package:exchange_app/src/cubits/auth/auth_cubit.dart';
+import 'package:exchange_app/src/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+
+final providers = [
+  BlocProvider<AuthCubit>(create: (context) => getIt<AuthCubit>()..check()),
+];
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appRouter = AppRouters();
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp.router(
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter.config(),
+    return MultiBlocProvider(
+      providers: providers,
+      child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        theme: lightTheme,
+        // darkTheme: darkTheme,
+        // themeMode: ThemeMode.system,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const HomeScreen(),
+        },
+      ),
     );
   }
 }
