2a39cc52db3f83eb3ebd0dd77273c5e024e9b852
Roman Diakonov <vegappunk@gmail.com>
2025-04-09 15:13:42 +0300

    fix: android bundle name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "fun.rony.exchange_app"
+    namespace = "ru.rodrix.exchangeapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "fun.rony.exchange_app"
+        applicationId = "ru.rodrix.exchangeapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
