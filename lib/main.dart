5f6316160d21b63924a713564b03cc7834f20712
Roman Diakonov <vegappunk@gmail.com>
2025-04-09 13:20:55 +0300

    fix: order of call

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:exchange_app/core/di/injectable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-main() async {
-  await dotenv.load(fileName: ".env");
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await configureInjection();
   runApp(const App());
 }
